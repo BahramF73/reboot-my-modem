08d4a6cb8b945474e11a6f740c17536484713199
bahram <bahram.0098.bf@gmail.com>
2025-04-25 15:50:02 +0300

    Improve comments for clarity and add alert handling in modem reboot script

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 
-#put the access url of your modem here
+# Put the access url of your modem here
 driver.get("http://192.168.0.1/login.html")
 
 # Enter login password
-# replace your modem password with "admin"
-driver.find_element(By.ID, "login-password").send_keys("admin")
+driver.find_element(By.ID, "login-password").send_keys("admin") # Replace your modem password with "admin"
 
 # Click "Login" button
 driver.find_element(By.ID, "save").click()
@@ -22,16 +21,20 @@ driver.find_element(By.ID, "save").click()
 # Set wait timer to 5 seconds
 wait = WebDriverWait(driver, 5)
 
-# wait and check if modem settings is loaded ( "Administration" panel button is shown)
+# Wait and check if modem settings is loaded ( "Administration" panel button is shown)
 admin_panel = WebDriverWait(driver, 200).until(
     expected_conditions.visibility_of_element_located((By.ID, "sub-menu"))).find_element(By.ID, "system")
 
-#click "Administration" panel
+# Click "Administration" panel
 admin_panel.click()
 
-# wait and check if "Administration" panel is loaded ("Reboot" button is shown)
+# Wait and check if "Administration" panel is loaded ("Reboot" button is shown)
 reboot_button = WebDriverWait(driver, 200).until(
     expected_conditions.visibility_of_element_located((By.NAME, "reboot")))
 
-# click "Reboot" button
+# Click "Reboot" button
 driver.execute_script("arguments[0].click();", reboot_button)
+
+# Click on JavaScript alert (popup)
+alert = driver.switch_to.alert
+alert.accept()
